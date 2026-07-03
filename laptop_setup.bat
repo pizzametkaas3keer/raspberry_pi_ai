cebb56de06b9bc0b114feafb819430bee5a29556
somebody <pizzametkaas3keer@gmail.com>
2026-07-03 11:11:47 +0200

    Update virtual environment setup in laptop_setup.bat

--- laptop_setup.bat
+++ laptop_setup.bat
@@ -1,4 +1,4 @@
-py -3.11 -m venv .venv311
-.\.venv311\Scripts\activate
+py -3.11 -m venv venv
+.\venv\Scripts\activate
 python -m pip install --upgrade pip
 pip install faster-whisper websockets sounddevice pyttsx3 numpy
